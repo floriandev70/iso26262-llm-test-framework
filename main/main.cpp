d81c9f0de9a7337765f3990617e3f35b9bc04190
Florian <139658946+floriandev70@users.noreply.github.com>
2025-08-30 18:56:22 +0200

    Update main.cpp
    error handling

--- main/main.cpp
+++ main/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char **argv) {
     std::cout << "Tests start now..." << std::endl;
     // prints
     dcdc_ntc_temp_def my_ntc_temp;
-    std::cout << Temp_Calc_Pres_Wacol_Temp_48V(&my_ntc_temp) << std::endl;
-    ::testing::InitGoogleTest(&argc, argv);
+    //std::cout << Temp_Calc_Pres_Wacol_Temp_48V(&my_ntc_temp) << std::endl;
+    //::testing::InitGoogleTest(&argc, argv);
     //::testing::GTEST_FLAG(filter) = "NOT_THERE.*";
     return RUN_ALL_TESTS();
 }
